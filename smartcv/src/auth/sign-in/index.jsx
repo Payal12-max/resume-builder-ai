3c92639829b70555449c432c0de9b021e23c80b4
Payal12-max <payalsulaniya88@gmail.com>
2026-07-03 00:21:13 +0530

    neccesary changes in UI and database

--- smartcv/src/auth/sign-in/index.jsx
+++ smartcv/src/auth/sign-in/index.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 function SignInPage() {
   return (
-    <div className='flex justify-center my-20 items-center'>
+    <div className=' bg-purple-50 flex justify-center my-20 items-center'>
       <SignIn/>
     </div>
   )
--- smartcv/src/components/custom/Header.jsx
+++ smartcv/src/components/custom/Header.jsx
@@ -7,7 +7,7 @@ import { UserButton, useUser } from '@clerk/clerk-react'
 function Header() {
     const { user, isSignedIn } = useUser();
     return (
-        <div className='p-3 px-5 flex justify-between shadow-md'>
+        <div className='p-3 px-5 10 backdrop-blur-md border-b border-white flex justify-between shadow-md'>
              <Link to={'/dashboard'}>
             <img src='/logo.svg' className='cursor-pointer' width={100} height={100} />
             </Link>
